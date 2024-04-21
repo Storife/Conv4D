77df5e0b7fbfa1291240110f405c4ee4975f674c
Ziqin Wang <782908504@qq.com>
2024-04-22 00:48:48 +0800

    conv4d

---
+++ Conv4D.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+
+
+
+
+
+
+class Conv4D(nn.Module):
+    def __init__(self, cin, cout, k, s=1, pad='same', dilation=1, g=1, b=True, pmode='zeros', device=None, dtype=None,):
+        super().__init__()
+        # self.weight = nn.Parameter(torch.zeros(cout, cin * k * k * k, dtype=dtype, device=device))
+        self.weight = nn.Parameter(torch.zeros(1, cout, cin * k * k * k, dtype=dtype, device=device))
+        if b:
+            self.bias = nn.Parameter(torch.zeros(cout, dtype=dtype, device=device))
+        self.cout = cout
+        self.k = k
+        self.s = s
+        self.pad = pad
+        self.dilation = dilation
+        self.g = g
+        self.pmode = pmode
+
+    def forward(self, x):
+        b, c, h, w, l = x.size()
+        x = unfoldNd(x, self.k, self.dilation, self.pad, self.s)#.view(b, c * k * k * k, h * w * l)
+        x = torch.matmul(self.weight, x).view(b, self.cout)
+        return x
+
+
+
+
+
+
+
+
+
+
